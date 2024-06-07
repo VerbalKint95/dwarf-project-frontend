b73844d521624f90b6b9a9771d378435b5b7874f
VerbalKint95 <constantin@cy-tech.fr>
2024-06-07 04:08:29 -0500

    .

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { isAuthenticated, getUserRole, isAdmin } from '../../utils/auth';
+import { useEffect} from 'react';
+import {isAdmin } from '../../utils/auth';
 
 const AdminIndexPage = () => {
   const router = useRouter();
